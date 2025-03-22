1a19982420570a02a38c3e44a4063f3bbdf5c4d9
Tyson <mr.j.aka.puddin@gmail.com>
2025-03-22 03:21:12 -0400

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,6 +3,11 @@ function increaseCount() {
   count++; // Increment the count by 1
   displayCount(); // Display the count
   checkCountValue(); // Check count value and display messages
+}  
+function resetCount() {
+    count = 0; // Reset count to 0
+    displayCount(); // Display the count
+    checkCountValue(); //Check count value and display messages
 }
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
@@ -11,6 +16,8 @@ function checkCountValue() {
   if (count === 10) {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
-    alert("Your Instagram post gained 20 followers! Keep it up!");
+    alert("Your Instagram post gained 20 followers! Keep it up!");  
+  } else if (count === 0) {
+    alert("Follower count has been reset");
   }
 }
